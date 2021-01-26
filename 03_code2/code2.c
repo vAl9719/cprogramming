d779ce91f2a90163bf63edd8431a8b5a6f386f35
Coursera Learner <nobody@nowhere.nul>
2021-01-26 02:57:26 +0000

    triangle fix

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -27,13 +27,14 @@ int main(void) {
   numStars = printTriangle(4);
   printf("That triangle had %d total stars\n", numStars);
   //now print "Here is a triangle with height 7\n"
-
+  printf("Here is a triangle with height 7\n");
   //then call printTriangle, passing in 7, and assign the result to numStars
-
+  numStars = printTriangle(7);
   //finally, print "That triangle had %d total stars\n", such that the %d 
   //prints the value of numStars
+  printf("That triangle had %d total stars\n", numStars);
 
-
+  
   return 0;
 }
 
